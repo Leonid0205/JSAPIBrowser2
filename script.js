743ff56dbce2902c61dad91ecec09759e4f53967
Leonid <leosb@mail.ru>
2024-02-11 11:52:39 +0300

    Some changes

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
 const sliderEl = document.querySelector('.slider');
 const previousButtonEl = document.querySelector('.previous__button');
 const nextButtonEl = document.querySelector('.next__button');
+const dotsNav = document.querySelector('.slider__nav');
 
-const slides = Array.from(sliderEl.querySelectorAll('img'))
+const slides = Array.from(sliderEl.querySelectorAll('img'));
+const dots = Array.from(dotsNav.querySelectorAll('button'));
 const slideCount = slides.length;
 let sliderIndex = 0;
 
 previousButtonEl.addEventListener('click', previousSlide);
 nextButtonEl.addEventListener('click', nextSlide);
+dotsNav.addEventListener('click', function (e) {
+    const targetDot = e.target.closest('button');
+    if (!targetDot) {
+        return
+    }
+
+    const targetIndex = dots.findIndex(dot => dot === targetDot);
+    
+    console.log(dots);
+    console.log(targetIndex);
+    renderSlide();
+    dots.forEach((dot, index, dots) => {
+        if (index === targetIndex) {
+            dots[index].classList.add('active');
+        } else {
+            dots[index].classList.remove('active');
+        }
+
+    });
+    slides.forEach((slide, index) => {
+        if (index === targetIndex) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+});
 
 function previousSlide() {
     sliderIndex = (sliderIndex - 1 + slideCount) % slideCount;
@@ -30,5 +59,13 @@ function renderSlide() {
         } else {
             slide.style.display = 'none';
         }
+    });
+    dots.forEach((dot, index, dots) => {
+        if (index === sliderIndex) {
+            dots[index].classList.add('active');
+        } else {
+            dots[index].classList.remove('active');
+        }
+
     });
 }
